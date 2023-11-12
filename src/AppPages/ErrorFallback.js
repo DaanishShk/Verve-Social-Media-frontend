148888fd18b3f390eae5607a2e5bc965baf8240a
Daanish Shaikh <84633644+DaanishShk@users.noreply.github.com>
2023-11-13 01:29:55 +0530

    fallback styling

--- src/AppPages/ErrorFallback.js
+++ src/AppPages/ErrorFallback.js
@@ -1,13 +1,16 @@
-import { FaSadCry } from "react-icons/fa";
-import { MdErrorOutline } from "react-icons/md";
-import { Navigate } from "react-router-dom";
+import { ImSad } from "react-icons/im";
+import "./css/ErrorFallback.css";
 
-function ErrorFallback({ error,resetErrorBoundary }) {
+function ErrorFallback({ error, resetErrorBoundary }) {
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={() => window.location.href = '/'}>Go back</button>
+    <div className="errorFallback">
+      <div className="errorFallback__wrapper">
+        <ImSad style={{ fontSize: "4em" }} />
+        <h1>Something went wrong</h1>
+        {/* <pre>{error.message}</pre> */}
+        <p>Click the button below to return to your feed</p>
+        <button onClick={() => (window.location.href = "/")}>Go back</button>
+      </div>
     </div>
   );
 }
